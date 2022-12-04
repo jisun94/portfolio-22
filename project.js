52c321856f1f5ebb7fe434f3da26d24120b97adf
jisun94 <jisun94lee@gmail.com>
2022-12-05 00:41:58 +0900

    ✨ Complete project.html

--- project.js
+++ project.js
@@ -19,3 +19,25 @@ const project = document.querySelectorAll('.project-page-contents');
 project.forEach((element) => {
   observer.observe(element);
 });
+
+// Cursor
+const cursor = () => {
+  const cursor = document.querySelector('.cursor');
+  const a = document.querySelectorAll('a');
+
+  document.addEventListener('mousemove', (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
+  });
+
+  a.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+      cursor.classList.add('hover');
+    });
+    item.addEventListener('mouseleave', () => {
+      cursor.classList.remove('hover');
+    });
+  });
+};
+cursor();
